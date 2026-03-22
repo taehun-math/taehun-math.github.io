519e3aeeba86b6670f3b37169e188927fc702fec
taehun-math <taehun.fields@gmail.com>
2026-03-22 16:40:53 +0000

    Deploying to gh-pages from @ taehun-math/taehun-math.github.io@35a8e35f861ce236898c5d56e798019651f2c603 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -17,7 +17,7 @@ ninja.data = [{
             window.location.href = "/publications/";
           },
         },{id: "nav-talks",
-          title: "Talks",
+          title: "talks",
           description: "Invited talks and conference presentations",
           section: "Navigation",
           handler: () => {
